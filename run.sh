4706ac3a9f167bfcf3cfbfeb238ea109ac6de662
Gregory Daynes <gregdaynes@gmail.com>
2017-01-11 14:09:47 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ for i in "${!array[@]}"
 do
     # echo ${array[i]};
     echo "CREATE DATABASE ${array[i]}" | mysql -u root -p$MYSQL_ROOT_PASSWORD
-    echo "GRANT ALL PRIVILEGES ON ${array[i]}.* TO 'user'@'%'" IDENTIFIED BY "$MYSQL_PASSWORD" | mysql -u root -p$MYSQL_ROOT_PASSWORD
-    echo "GRANT ALL PRIVILEGES ON ${array[i]}.* TO 'user'@'localhost'" INDENTIFIED BY "$MYSQL_PASSWORD" | mysql -u root -p$MYSQL_ROOT_PASSWORD
+    echo "GRANT ALL PRIVILEGES ON ${array[i]}.* TO 'user'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -u root -p$MYSQL_ROOT_PASSWORD
+    echo "GRANT ALL PRIVILEGES ON ${array[i]}.* TO 'user'@'localhost' INDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -u root -p$MYSQL_ROOT_PASSWORD
 done
